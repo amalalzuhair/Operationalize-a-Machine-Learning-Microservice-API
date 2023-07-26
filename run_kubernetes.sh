c9337812791281adb5e8a3e0032dd8f2ffbb7680
Amal <amal.alzuhair7@gmail.com>
2023-07-26 03:21:04 +0300

    updating app.py, readme, requirements, run_docker, upload_docker,

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="amalsz/dockermlproject:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run dockermlproject\
+    --image=$dockerpath\
+    --port=80 --labels app=dockermlproject
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward dockermlproject 8000:80
 
